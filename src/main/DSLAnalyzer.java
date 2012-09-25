47353ff70e3b9eac7c1127482d7f5f1711f1035b
cuipengfei <cuipengfei2008@gmail.com>
2012-09-25 21:13:16 +0800

    support scenario 2 now

--- src/main/DSLAnalyzer.java
+++ src/main/DSLAnalyzer.java
@@ -1,21 +1,85 @@
 package main;
 
-/**
- * Created with IntelliJ IDEA.
- * User: twer
- * Date: 9/8/12
- * Time: 2:45 AM
- * To change this template use File | Settings | File Templates.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DSLAnalyzer {
-    public static String analyze(String inputCode) {
+    public static String analyze(String inputCode) throws IOException {
         String result = null;
         if (isSingleMethod(inputCode)) {
             result = inputCode + ": ROOT\n"
-                    + "Method(s) number: 1"
-            ;
+                    + "Method(s) number: 1";
+        } else if (isNested(inputCode)) {
+            String rootMethodName = getRootMethodName(inputCode);
+            HashMap<String, String> nestedMethodNames = getNestedMethodNames(inputCode);
+
+            result = rootMethodName + ": ROOT\n";
+            for (Map.Entry<String, String> callerCalleePair : nestedMethodNames.entrySet()) {
+                result += (">" + callerCalleePair.getValue() + ": " + "ROOT -> " + callerCalleePair.getKey() + "\n");
+            }
+
+            Integer methodsNumber = nestedMethodNames.size() + 1;
+            result += "Method(s) number: " + methodsNumber.toString();
         }
         return result;
+    }
+
+    private static HashMap<String, String> getNestedMethodNames(String inputCode) throws IOException {
+        StringReader stringReader = new StringReader(inputCode);
+        BufferedReader bufferedReader = new BufferedReader(stringReader);
+
+        HashMap<String, String> results = new HashMap<String, String>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (isMethodCall(line)) {
+                results.put(getMethodName(line).getKey(), getMethodName(line).getValue());
+            }
+        }
+        return results;
+    }
+
+    private static Map.Entry<String, String> getMethodName(String line) {
+        String[] parts = line.split("\\.");
+        final String caller = parts[0];
+
+        String secondPart = parts[1];
+        final String callee = secondPart.replace("(", "").replace(")", "").replace(";", "");
+
+        return new Map.Entry<String, String>() {
+            @Override
+            public String getKey() {
+                return caller;
+            }
+
+            @Override
+            public String getValue() {
+                return callee;
+            }
+
+            @Override
+            public String setValue(String s) {
+                return null;
+            }
+        };
+    }
+
+    private static boolean isMethodCall(String line) {
+        return line.contains(".") && line.contains(";");
+    }
+
+    private static String getRootMethodName(String inputCode) throws IOException {
+        StringReader stringReader = new StringReader(inputCode);
+        BufferedReader bufferedReader = new BufferedReader(stringReader);
+        String firstLine = bufferedReader.readLine();
+        bufferedReader.close();
+        return firstLine.replace("{", "");
+    }
+
+    private static boolean isNested(String inputCode) {
+        return !isSingleMethod(inputCode);
     }
 
     private static boolean isSingleMethod(String inputCode) {
--- src/test/DSLAnalyzerTest.java
+++ src/test/DSLAnalyzerTest.java
@@ -3,15 +3,26 @@ package test;
 import main.DSLAnalyzer;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class DSLAnalyzerTest {
     @Test
-    public void should_return_single_result_when_input_is_one_method() {
+    public void should_return_single_result_when_input_is_one_method() throws IOException {
         String result = DSLAnalyzer.analyze("method0");
         assertThat(result, is("method0: ROOT\n" +
                 "Method(s) number: 1"));
     }
 
+    @Test
+    public void should_return_nested_result_when_input_is_nested_with_one_method() throws IOException {
+        String result = DSLAnalyzer.analyze("method0{\n" +
+                "A.method1();\n" +
+                "}");
+        assertThat(result, is("method0: ROOT\n" +
+                ">method1: ROOT -> A\n" +
+                "Method(s) number: 2"));
+    }
 }
