e44ffa05e1a55103e98c295a032241e8079092ed
cuipengfei <cuipengfei2008@gmail.com>
2012-09-25 21:21:08 +0800

    support scenario 3 now

--- src/test/DSLAnalyzerTest.java
+++ src/test/DSLAnalyzerTest.java
@@ -25,4 +25,16 @@ public class DSLAnalyzerTest {
                 ">method1: ROOT -> A\n" +
                 "Method(s) number: 2"));
     }
+
+    @Test
+    public void should_return_nested_result_when_input_is_nested_with_two_method() throws IOException {
+        String result = DSLAnalyzer.analyze("method0{\n" +
+                "A.method1();\n" +
+                "B.method2();\n" +
+                "}");
+        assertThat(result, is("method0: ROOT\n" +
+                ">method1: ROOT -> A\n" +
+                ">method2: ROOT -> B\n" +
+                "Method(s) number: 3"));
+    }
 }
