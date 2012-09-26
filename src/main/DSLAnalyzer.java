aa214f16f7d1ac897bac9ae6df4cef434f0f836d
cuipengfei <cuipengfei2008@gmail.com>
2012-09-26 23:43:30 +0800

    further remove redundancy and extract

--- src/main/DSLAnalyzer.java
+++ src/main/DSLAnalyzer.java
@@ -7,40 +7,40 @@ import java.util.Map;
 
 public class DSLAnalyzer {
     public static String analyze(String inputCode) throws IOException {
-        String rootMethodName = getRootMethodName(inputCode);
-
-        DSLNode currentNode = new DSLNode();
-        currentNode.setCalleeName(rootMethodName);
-
+        DSLNode currentNode = createRootNode(inputCode);
         StringReader stringReader = new StringReader(inputCode);
         BufferedReader bufferedReader = new BufferedReader(stringReader);
         String line;
         while ((line = bufferedReader.readLine()) != null) {
+            if (isMethodCall(line)) {
+                DSLNode node = createNewNode(line);
+                currentNode.addSubNode(node);
+                if (line.contains("{")) {
+                    currentNode = node;
+                }
+            }
             if (line.contains("}") && currentNode.getParentNode() != null) {
                 currentNode = currentNode.getParentNode();
-            }
-            if (isMethodCall(line)) {
-                if (line.contains("{")) {
-                    DSLNode node = new DSLNode();
-                    Map.Entry<String, String> callerCalleePair = getMethodName(line);
-                    node.setCallerName(callerCalleePair.getKey());
-                    node.setCalleeName(callerCalleePair.getValue());
-
-                    currentNode.addSubNode(node);
-                    currentNode = node;
-                } else {
-                    DSLNode node = new DSLNode();
-                    Map.Entry<String, String> callerCalleePair = getMethodName(line);
-                    node.setCallerName(callerCalleePair.getKey());
-                    node.setCalleeName(callerCalleePair.getValue());
-
-                    currentNode.addSubNode(node);
-                }
             }
         }
 
         String result = currentNode.toString();
         return result;
+    }
+
+    private static DSLNode createRootNode(String inputCode) throws IOException {
+        String rootMethodName = getRootMethodName(inputCode);
+        DSLNode currentNode = new DSLNode();
+        currentNode.setCalleeName(rootMethodName);
+        return currentNode;
+    }
+
+    private static DSLNode createNewNode(String line) {
+        DSLNode node = new DSLNode();
+        Map.Entry<String, String> callerCalleePair = getMethodName(line);
+        node.setCallerName(callerCalleePair.getKey());
+        node.setCalleeName(callerCalleePair.getValue());
+        return node;
     }
 
     private static Map.Entry<String, String> getMethodName(String line) {
